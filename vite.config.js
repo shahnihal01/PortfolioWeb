03353c0f936b6806d84a00c4f933a6574d4a80bf
Nihal Shah <82139908+shahnihal01@users.noreply.github.com>
2022-06-25 14:58:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,21 +11,22 @@ export default defineConfig({
       manifest: {
         name: 'Nihal Shah',
         short_name: 'NSRES',
+        start_url:'https://nihal-shah.vercel.app/',
         description: 'Nihals Portfolio',
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'pwa-192x192.png',
+            src: '/pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png',
+            src: '/pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
